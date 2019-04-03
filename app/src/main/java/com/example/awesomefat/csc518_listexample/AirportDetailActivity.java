1b4a816cefb774c3a6a5904fdd9a06e384a39891
Mike Litman <awesomefat@gmail.com>
2019-04-02 19:16:02 -0500

    added code for hw

--- app/src/main/java/com/example/awesomefat/csc518_listexample/AirportDetailActivity.java
+++ app/src/main/java/com/example/awesomefat/csc518_listexample/AirportDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.awesomefat.csc518_listexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,28 +14,46 @@ import java.util.LinkedList;
 public class AirportDetailActivity extends AppCompatActivity
 {
     private ListView destinationsLV;
+    private AirportDetailActivity myself;
+    private LinkedList<String> ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.myself = this;
         setContentView(R.layout.activity_airport_detail);
-        TextView airportTV = this.findViewById(R.id.airportTV);
+        final TextView airportTV = this.findViewById(R.id.airportTV);
         this.destinationsLV = this.findViewById(R.id.destinationsLV);
 
-        LinkedList<String> ll = new LinkedList<String>();
-        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.another_row, ll);
+        this.ll = new LinkedList<String>();
+        final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.another_row, ll);
         this.destinationsLV.setAdapter(aa);
 
+        this.destinationsLV.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long row_id)
+            {
+                String selectedAirport = ll.get(position);
+                String[] parts = selectedAirport.split(" ");
+
+                Intent i = new Intent(myself, AirportDetailActivity.class);
+                i.putExtra("airportCode", parts[parts.length-1].trim());
+                myself.startActivity(i);
+            }
+        });
+
+
         String airportCode = this.getIntent().getStringExtra("airportCode");
-
-        //strip the " from both ends of the airport code
         airportCode = airportCode.replaceAll("\"","");
-
-
         airportTV.setText(airportCode);
         NetworkThread nt = new NetworkThread(airportCode, aa, ll);
         nt.setPriority(Thread.MAX_PRIORITY);
         nt.start();
+
+        //strip the " from both ends of the airport code
+
+
     }
 }
